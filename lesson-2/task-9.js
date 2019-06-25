af6cffdf9b081cc9b24f23ce4c3b78dff3a76453
iks3000 <khomyn@gmail.com>
2019-06-25 14:08:53 +0300

    descending sorting
    descending sorting

--- lesson-2/task-9.js
+++ lesson-2/task-9.js
@@ -1,4 +1,4 @@
-# Задача 9
+/*# Задача 9
 
 Отсортируйте массив по убыванию.
 
@@ -8,5 +8,15 @@
 const arr = [1, 2, 3, 4, 5, 6];
 // [6,5,4,3,2,1]
 ```
+*/
 
+const arr = [1, 2, 3, 4, 5, 6];
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; arr[i] > arr[j]; j++) {
+    let sort = arr[i];
+    arr[i] = arr[j];
+    arr[j] = sort;
+  }
+}   
+console.log(arr);
 
