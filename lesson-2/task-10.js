61e8fe3ac67b14486c36c12683df4fb932f655b9
iks3000 <khomyn@gmail.com>
2019-06-25 14:03:10 +0300

    Sort Ascending
    Sort Ascending

--- lesson-2/task-10.js
+++ lesson-2/task-10.js
@@ -1,4 +1,4 @@
-# Задача 10
+/*# Задача 10
 
 Отсортируйте массив по возрастанию.
 
@@ -8,3 +8,15 @@
 const arr = [6, 5, 4, 3, 2, 1];
 // [1,2,3,4,5,6]
 ```
+*/
+
+
+const arr = [6, 5, 4, 3, 2, 1];
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; arr[i] < arr[j]; j++) {
+    let sort = arr[i];
+    arr[i] = arr[j];
+    arr[j] = sort;
+  }
+}   
+console.log(arr);
