2f1ef68dd401a1b10d51d436aeeb66142e79abfa
iks3000 <khomyn@gmail.com>
2019-06-26 13:58:01 +0300

    Task-4

--- lesson-3/4.js
+++ lesson-3/4.js
@@ -12,6 +12,50 @@
 
 // Решение
 
+const f = function(day) {
+  const daysOfWeek = [
+    'Воскресенье',
+    'Понедельник',
+    'Вторник',
+    'Среда',
+    'Четверг',
+    'Пятница',
+    'Суббота'
+  ];
+  if (day < 0 || day > 7 ) {
+    throw new Error('parameter should be in the range of 1 to 7');
+  } else if (typeof(day) === 'number') {
+    for(let i = 0; i < daysOfWeek.length; i++) {
+      if (i === day) {
+        return daysOfWeek[i-1];
+      }
+    }
+  } else {
+    throw new Error('parameter type is not a Number');
+  }
+}const f = function(day) {
+  const daysOfWeek = [
+    'Воскресенье',
+    'Понедельник',
+    'Вторник',
+    'Среда',
+    'Четверг',
+    'Пятница',
+    'Суббота'
+  ];
+  if (day < 0 || day > 7 ) {
+    throw new Error('parameter should be in the range of 1 to 7');
+  } else if (typeof(day) === 'number') {
+    for(let i = 0; i < daysOfWeek.length; i++) {
+      if (i === day) {
+        return daysOfWeek[i-1];
+      }
+    }
+  } else {
+    throw new Error('parameter type is not a Number');
+  }
+}
+
 /* не удалять */
 f(1); // Воскресенье
 f(2); // Понедельник
