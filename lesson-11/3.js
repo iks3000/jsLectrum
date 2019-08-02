ac35f5aec8fe3af163844cd322e783d143d251ef
iks3000 <khomyn@gmail.com>
2019-08-02 10:55:09 +0300

    Create 3.js

---
+++ lesson-11/3.js
@@ -0,0 +1,33 @@
+/**
+ * Задача 3.
+ *
+ * Улучшите функцию createFibonacciGenerator() из предыдущего примера.
+ *
+ * Теперь вызов функции createFibonacciGenerator() должен возвращать объект, который содержит два метода:
+ * - print — возвращает число из последовательности Фибоначчи;
+ * - reset — обнуляет последовательность и ничего не возвращает.
+ *
+ * Условия:
+ * - Задачу нужно решить с помощью замыкания.
+ */
+
+// Решение
+
+const generator1 = createFibonacciGenerator();
+
+console.log(generator1.print()); // 1
+console.log(generator1.print()); // 1
+console.log(generator1.print()); // 2
+console.log(generator1.print()); // 3
+console.log(generator1.reset()); // undefined
+console.log(generator1.print()); // 1
+console.log(generator1.print()); // 1
+console.log(generator1.print()); // 2
+
+const generator2 = createFibonacciGenerator();
+
+console.log(generator2.print()); // 1
+console.log(generator2.print()); // 1
+console.log(generator2.print()); // 2
+
+exports.createFibonacciGenerator = createFibonacciGenerator;
