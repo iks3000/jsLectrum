1b9e96a69a9b45b7cc166b3fd59d14a4e82bf0a1
iks3000 <khomyn@gmail.com>
2019-08-02 10:53:55 +0300

    Create 1.js

---
+++ lesson-11/1.js
@@ -0,0 +1,40 @@
+/**
+ * Задача 1.
+ *
+ * Создайте функцию createNumberGenerator(),
+ * которая вернёт ещё одну функцию,
+ * каждый вызов которой будет генерировать и возвращать случайное число от 1 до 100 (не включая само число 100).
+ *
+ * Условия:
+ * - Числа не должны повторяться;
+ * - Задачу нужно решить с помощью замыкания.
+ *
+ * Генерировать ошибку если:
+ * - Функция была вызвана, когда доступные для выведения числа закончились.
+ *
+ * Подсказка: в замыкании можно хранить массив с числами, которые уже были созданы функцией.
+ */
+
+// Решение
+
+const TOTAL_ITERATIONS = 101;
+let invocations = 0;
+const generateNumber = createNumberGenerator();
+
+try {
+    for (let iteration = 1; iteration < TOTAL_ITERATIONS; iteration++) {
+        console.log(`Iteration: ${iteration}. Number: ${generateNumber()}`);
+        invocations += 1;
+    }
+} catch {
+    console.log('Error caught.');
+    console.log(
+        `Function generated an error after ${invocations} invocations.`,
+    );
+}
+
+// Когда все числа выведутся:
+// Error caught.
+// Function generated an error after 99 invocations.
+
+exports.createNumberGenerator = createNumberGenerator;
