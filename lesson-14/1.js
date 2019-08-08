f7261b4ef3f601193acef78ef6f03a83dca5df5a
iks3000 <khomyn@gmail.com>
2019-08-08 16:56:55 +0300

    Update 1.js

--- lesson-14/1.js
+++ lesson-14/1.js
@@ -18,38 +18,57 @@
  */
 
 function CleanerRobot(
-    initialEnergy = 0 /* Изначальный заряд батареи робота */,
-    cleaningSquare /* Площадь для уборки в метрах. */,
+  initialEnergy = 0 /* Изначальный заряд батареи робота */,
+  cleaningSquare /* Площадь для уборки в метрах. */,
 ) {
-    let energy = initialEnergy;
-    let timerId = 0;
-    const ENERGY_CONSUMPTION = 1; /* Расход энергии: 1% батареи на 1 час работы. */
-    const CLEANING_SPEED = 10; /* Скорость уборки: 10 квадратных метров в час. */
-    const getCleaningTime = () => cleaningSquare / CLEANING_SPEED;
-    const onReady = () =>
-        console.log(`Уборка завершена. Осталось заряда батареи: ${energy}.`);
+  let energy = initialEnergy;
+  let timerId = 0;
+  const ENERGY_CONSUMPTION = 1; /* Расход энергии: 1% батареи на 1 час работы. */
+  const CLEANING_SPEED = 10; /* Скорость уборки: 10 квадратных метров в час. */
+  const getCleaningTime = () => cleaningSquare / CLEANING_SPEED; /* Подсчет времени уборки */
+  const remainingPower = () => ENERGY_CONSUMPTION * energy - getCleaningTime(); /* Подсчет оставшейся энергии робота */
 
-    this.clean = () => {
-        const cleaningTime = getCleaningTime();
+  const onReady = () => {
+    console.log(`Cleaning completed. Battery remaining: ${remainingPower()}%`);
+    clearInterval(interval);
+  }
 
-        console.log(
-            `Начинаю процесс уборки. Время уборки: ${cleaningTime} часов.`,
-        );
 
-        /* Для удобства время уборки сокращено до формата 1 час = 1 секунда */
-        setTimeout(onReady, cleaningTime * 1000);
-    };
+  let timer;
+  let interval;
+  
+  this.clean = () => {
+    const cleaningTime = getCleaningTime();
 
-    // Решение
+    console.log(
+      `Start the cleaning process. Cleaning time: ${cleaningTime} ${(cleaningTime > 1) ? 'hours' : 'hour'}.`,
+    );
+
+    interval = setInterval( function () {
+      timerId++;
+      console.log('interval = ' + timerId)
+    }, 1000)
+
+    /* Для удобства время уборки сокращено до формата 1 час = 1 секунда */
+    timer = setTimeout(onReady, cleaningTime * 1000);
+  };
+
+  // Решение
+  this.stop = () => {
+    clearTimeout(timer);
+    clearInterval(interval);
+    let anticipatorily = ENERGY_CONSUMPTION * energy - timerId;
+    console.log(`After ${timerId} seconds: Cleaning completed ahead of schedule. Battery remaining: ${anticipatorily}%`);
+    timerId = 0;
+  };
 }
 
 const cleanerRobot = new CleanerRobot(50, 45);
 cleanerRobot.clean(); /* Начинаю процесс уборки. Время уборки: 4.5 часов. */
-
 /* Спустя 4.5 секунды: Уборка завершена. Осталось заряда батареи: 45.5. */
 
 // setTimeout(() => {
-//     cleanerRobot.stop(); /* Спустя 1 секунду: Уборка завершена досрочно. Осталось заряда батареи: 45.5. */
+//   cleanerRobot.stop(); /* Спустя 1 секунду: Уборка завершена досрочно. Осталось заряда батареи: 45.5. */
 // }, 1000);
 
 exports.CleanerRobot = CleanerRobot;
